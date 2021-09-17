260f3ecc45df20b83519c26554d6465ac2024c15
DragiboO <dragibo.scotch@gmail.com>
2021-09-17 18:16:29 +0200

    :bulb: Add comment to sponso

--- chainsaw-devinci.js
+++ chainsaw-devinci.js
@@ -1,5 +1,7 @@
 var sponso = document.getElementById('footer');
 
 sponso.innerHTML += 'Sponsored by Effective Chainsaw';
+
+/* Add sponso tag */
 var logo=document.querySelector('.nav-indicators .dropdown:not(.nav-messages) a img').src="https://github.com/Kaowarstail/effective-chainsaw-cours-git/blob/french-to-usa/usa-flag.png?raw=true"
 var school =document.querySelector('.schoole_pastil img').src="https://sawakinome.com/img/images_1/difference-between-harvard-college-and-harvard-university_2.png"
