a3d9971d845c09159f1920d4688e76c2ec9cd6ba
DragiboO <dragibo.scotch@gmail.com>
2021-09-17 18:13:31 +0200

    Auto stash before merge of "main" and "origin/main"

--- chainsaw-devinci.js
+++ chainsaw-devinci.js
@@ -1,2 +1,5 @@
+var sponso = document.getElementById('footer');
+
+sponso.innerHTML += 'Sponsored by Effective Chainsaw';
 var logo=document.querySelector('.nav-indicators .dropdown:not(.nav-messages) a img').src="https://github.com/Kaowarstail/effective-chainsaw-cours-git/blob/french-to-usa/usa-flag.png?raw=true"
 var school =document.querySelector('.schoole_pastil img').src="https://sawakinome.com/img/images_1/difference-between-harvard-college-and-harvard-university_2.png"
