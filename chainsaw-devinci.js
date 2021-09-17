65d0bd35eb52bd220ff8531918c868852aef4660
Drouet <quentin.drouet109@gmail.com>
2021-09-17 17:53:44 +0200

    🩹 Fix flag src and width

---
+++ chainsaw-devinci.js
@@ -0,0 +1,1 @@
+var logo=document.querySelector('.nav-indicators .dropdown:not(.nav-messages) a img').src="https://github.com/Kaowarstail/effective-chainsaw-cours-git/blob/french-to-usa/usa-flag.png?raw=true"
