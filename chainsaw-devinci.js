4d024e676794d4b3c32d26c092e20c4a5e423bb6
Kaowarstail <victor.dane@edu.devinci.fr>
2021-09-18 17:11:10 +0200

    💡Update comment in js

--- chainsaw-devinci.js
+++ chainsaw-devinci.js
@@ -2,8 +2,11 @@
 var sponso = document.getElementById('footer');
 sponso.innerHTML += 'Sponsored by Effective Chainsaw 🥓🥚';
 
-
+//Update flag
 var logo=document.querySelector('.nav-indicators .dropdown:not(.nav-messages) a img').src="https://github.com/Kaowarstail/effective-chainsaw-cours-git/blob/french-to-usa/usa-flag.png?raw=true";
 var school=document.querySelector('.schoole_pastil img').src="https://sawakinome.com/img/images_1/difference-between-harvard-college-and-harvard-university_2.png";
+
+//Update school logo
 var logo = document.querySelector('.nav-indicators .dropdown:not(.nav-messages) a img').src="https://github.com/Kaowarstail/effective-chainsaw-cours-git/blob/french-to-usa/usa-flag.png?raw=true";
 var school = document.querySelector('.schoole_pastil img').src="https://sawakinome.com/img/images_1/difference-between-harvard-college-and-harvard-university_2.png";
+
