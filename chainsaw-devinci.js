c3fe60a92d017aac00a28fd170d94eb92e1461f5
Kaowarstail <victor.dane@edu.devinci.fr>
2021-09-18 17:00:00 +0200

    ⚰️ Abort change logo

--- chainsaw-devinci.js
+++ chainsaw-devinci.js
@@ -7,9 +7,3 @@ var logo=document.querySelector('.nav-indicators .dropdown:not(.nav-messages) a 
 var school=document.querySelector('.schoole_pastil img').src="https://sawakinome.com/img/images_1/difference-between-harvard-college-and-harvard-university_2.png";
 var logo = document.querySelector('.nav-indicators .dropdown:not(.nav-messages) a img').src="https://github.com/Kaowarstail/effective-chainsaw-cours-git/blob/french-to-usa/usa-flag.png?raw=true";
 var school = document.querySelector('.schoole_pastil img').src="https://sawakinome.com/img/images_1/difference-between-harvard-college-and-harvard-university_2.png";
-
-//try changing burger menu logo
-var logo_burger = document.createElement("img");
-logo_burger.setAttribute("src", "https://github.com/Kaowarstail/effective-chainsaw-cours-git/blob/main/chainsaw-logo.png?raw=true")
-var square_burger = document.getElementById(".container-fluid .");
-square_burger.src = logo_burger;
