e7c6c67c87c5af408b57fab5a6cfc9a0d6424f2d
Kaowarstail <victor.dane@edu.devinci.fr>
2021-09-19 19:48:06 +0200

    💄Update user avatar + user name

--- chainsaw-devinci.js
+++ chainsaw-devinci.js
@@ -8,3 +8,7 @@ var logo = document.querySelector('.nav-indicators .dropdown:not(.nav-messages) 
 //Update school logo
 var school = document.querySelector('.schoole_pastil img').src = "https://sawakinome.com/img/images_1/difference-between-harvard-college-and-harvard-university_2.png";
 
+//Chainsaw logo 
+var chainsaw = document.querySelector(".user img").src = "https://github.com/Kaowarstail/effective-chainsaw-cours-git/blob/main/chainsaw-logo.png?raw=true";
+//Change your name into a better one
+var better_name = document.querySelector(".user span").textContent = "Effective Chainsaw";
